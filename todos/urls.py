73f4f30e18136bd1a4c4650839ad16bd469a3ac2
Flor Subillaga <subillagaflor@gmail.com>
2025-02-15 18:01:21 -0400

    detail view app

--- todos/urls.py
+++ todos/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ListTodoItem
+from .views import ListTodoItem, DetailTodoItem
 
 urlpatterns = [
 
-    path('',ListTodoItem.as_view(), name='todo_list')
+    path('',ListTodoItem.as_view(), name='todo_list'),
+    path('<int:pk>/',DetailTodoItem.as_view(), name = 'todo_detail'),
+
 ]
